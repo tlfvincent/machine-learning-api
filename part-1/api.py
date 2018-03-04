81249285911a8868d588d50e1bc635b12b0aee67
Thomas Vincent <thomasvincent@Thomass-MBP.fios-router.home>
2018-03-04 18:35:11 -0500

    add working api

--- part-1/api.py
+++ part-1/api.py
@@ -9,13 +9,15 @@ class IrisService(Flask):
         pickle_path = "./models/model.pickle"
         with open(pickle_path, 'rb') as handle:
             clf = pickle.load(handle)
+            features = pickle.load(handle)
 
-        return clf
+        print(features)
+        return clf, features
 
     def __init__(self, *args, **kwargs):
             super(IrisService, self).__init__(*args, **kwargs)
             # load sketchy classifier in memory
-            self.classifier = self.load_classifier()
+            self.classifier, self.features = self.load_classifier()
 
 
 iris_service = IrisService(__name__)
@@ -30,7 +32,7 @@ def index():
 
 
 @iris_service.route("/predict", methods=['POST'])
-def get_predictions(data):
+def get_predictions():
     """
     On request, this returns a list of ``ndata`` randomly made data points.
     :param ndata: (optional)
@@ -38,15 +40,23 @@ def get_predictions(data):
     :returns data:
         A JSON string of ``ndata`` data points.
     """
-    test_json = request.get_json()
-    test = pd.read_json(test_json, orient='records')
-    predictions = iris_service.classifier.predict(test)
+    #return "Hello World! My Hostname is: {0}".format(socket.gethostname())
+    json_ = request.json
+    query = pd.get_dummies(pd.DataFrame.from_records([json_]))
+    query = query.reindex(columns=iris_service.features, fill_value=0)
 
-    response = jsonify(predictions)
-    response.status_code = 200
+    probas_ = iris_service.classifier.predict_proba(query)
+    predictions = {'Iris-setosa': probas_[0][0],
+                   'Iris-versicolor': probas_[0][1],
+                   'Iris-virginica': probas_[0][2]}
 
+    response = jsonify({'prediction': predictions, "status_code": 200})
     return response
 
-if __name__ == "__main__":
-    iris_service.run(host='0.0.0.0', port="5000")
 
+
+    # response = jsonify({'prediction': predictions, "status_code": 200})
+    # return response
+
+if __name__ == "__main__":
+    iris_service.run(host='0.0.0.0', port=5010, debug=True)
