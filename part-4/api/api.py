744e88e95bbf2cbcd426a2a467d66bac1fbaccdd
Thomas Vincent <thomasvincent@Thomass-MBP.fios-router.home>
2018-03-10 17:02:58 -0500

    add DB rollback

--- part-4/api/api.py
+++ part-4/api/api.py
@@ -1,6 +1,8 @@
+import time
 import socket
 import pickle
 import traceback
+import datetime
 import pandas as pd
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -28,7 +30,36 @@ setup_metrics(iris_service)
 iris_service.config.from_object(BaseConfig)
 db = SQLAlchemy(iris_service)
 
-from schemas import *
+
+class Classifications(db.Model):
+
+    __tablename__ = 'classifications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    payload = db.Column(db.String, nullable=True)
+    response = db.Column(db.String, nullable=True)
+    status_code = db.Column(db.String, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True)
+
+    def __init__(self, payload, response, status_code):
+        self.payload = payload
+        self.response = response
+        self.status_code = status_code
+        self.created_at = datetime.datetime.now()
+
+
+def database_initialization_sequence():
+    db.create_all()
+    test_rec = Classifications(
+               '{"sepal_length": 1, "sepal_width":0.2, "petal_length":3, "petal_width":2}',
+               '{"Iris-setosa": 0.5, "Iris-versicolor":0.25, "Iris-virginica":0.34}',
+               'success')
+
+    db.session.add(test_rec)
+    db.session.rollback()
+    db.session.commit()
+
+#from schemas import *
 
 
 @iris_service.route("/")
@@ -75,4 +106,15 @@ def get_predictions():
 
 
 if __name__ == "__main__":
+    dbstatus = False
+    while dbstatus is False:
+        try:
+            db.create_all()
+        except:
+            time.sleep(2)
+        else:
+            dbstatus = True
+
+    database_initialization_sequence()
+
     iris_service.run(host='0.0.0.0', port=5000, debug=True)
