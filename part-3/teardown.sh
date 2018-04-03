506bf046bd42884d12e9ae8a60a980e05f675aa1
Thomas Vincent <tlfvincent@gmail.com>
2018-04-03 02:27:14 +0000

    remove db and data containers

--- part-3/teardown.sh
+++ part-3/teardown.sh
@@ -8,12 +8,6 @@ docker-compose down
 docker-compose stop api
 docker-compose rm api
 
-docker-compose stop db
-docker-compose rm db
-
-docker-compose stop data
-docker-compose rm data
-
 docker-compose stop loadbalancer
 docker-compose rm loadbalancer
 
